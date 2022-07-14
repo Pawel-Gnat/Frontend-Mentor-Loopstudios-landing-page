99b87952ee29b1d8019cc5252a29125c6d7da4ec
Paweł <pawel_gnat@o2.pl>
2022-07-14 21:05:10 +0200

    improved my js file, less code

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 const menuBtn = document.querySelector('.menu__btn')
 const animateBtn = document.querySelector('.active')
-const firstLine = document.querySelector('.menu__btn--line:nth-child(1)')
-const secondLine = document.querySelector('.menu__btn--line:nth-child(2)')
-const thirdLine = document.querySelector('.menu__btn--line:nth-child(3)')
 const displayLinks = document.querySelector('.display')
 const linksBox = document.querySelector('.menu__box')
 const body = document.querySelector('body')
 const disableScroll = document.querySelector('.overflow')
 const menuLinks = document.querySelectorAll('.menu__box--links a')
+const buttonLines = Array.from(document.querySelectorAll('.menu__btn--line')) // an array with my button menu lines
 
-menuBtn.addEventListener('click', () => {
-	firstLine.classList.toggle('active')
-	secondLine.classList.toggle('active')
-	thirdLine.classList.toggle('active')
+function toggleActive() {
+	// function animate my hamburger icon, show mobile menu and prevent vertical scroll
+	buttonLines.forEach(
+		// function triggers my array elements (hamburger menu lines) and animate them
+		line => {
+			line.classList.toggle('active')
+		}
+	)
 	linksBox.classList.toggle('display')
 	body.classList.toggle('overflow')
-})
+}
 
 menuLinks.forEach(links => {
+	// loop function that closes my mobile menu view while I select one of website links
 	links.addEventListener('click', () => {
-		firstLine.classList.toggle('active')
-		secondLine.classList.toggle('active')
-		thirdLine.classList.toggle('active')
+		buttonLines.forEach(line => {
+			line.classList.toggle('active')
+		})
 		linksBox.classList.toggle('display')
 		body.classList.toggle('overflow')
 	})
 })
+
+menuBtn.addEventListener('click', toggleActive)
